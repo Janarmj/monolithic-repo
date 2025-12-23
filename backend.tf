415b80472440166ce328802d5f04cf97ca2c8382
Janarmj <janardhan6304mj@gmail.com>
2025-12-23 16:00:50 +0530

    Change S3 bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 terraform {
 backend "s3" {
 region = "us-west-1"
-bucket = "shaikmustafa77.flm.devsecops.project.bucket"
+bucket = "janamj.flm.devsecops.project.bucket"
 key = "prod/terraform.tfstate"
 }
 }
